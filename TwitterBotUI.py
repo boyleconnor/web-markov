50d28379a0239451d25c394180febc797034163f
Ayoub Belemlih <abelemli@macalester.edu>
2016-11-15 10:47:05 -0600

    Added tweets file with tweets from some of the 100 most followed users

--- TwitterBotUI.py
+++ TwitterBotUI.py
@@ -1,1 +1,2 @@
 #tweetDatabase_tech: sample of tweets from Bill Gates, Satya Nadella, and others.
+#tweetDataBase_100mostFollowed: sample of tweets from Katy Perry, Justin Bieber, Barack Obama, Youtube
