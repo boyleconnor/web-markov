0a288785c6b25c54057b3b75a1e421d33ad8e0fd
Ayoub Belemlih <abelemli@macalester.edu>
2016-11-20 18:40:17 -0600

    Added network

--- onechains.py
+++ onechains.py
@@ -61,38 +61,38 @@ for i in range(len(tokens)-1):
     chains.add_edge(tokens[i], tokens[i+1])
 
 
-# def gen_text(graph, max_length=100):
-#     node = START_TOKEN
-#     text = ''
-#     count = 0
-#     while node != END_TOKEN and count < max_length:
-#         count += 1
-#         if node != START_TOKEN:
-#             text += node+' '
-#         children = graph.get_neighbors(node)
-#         total_weight = 0
-#         die = []
-#         for child, weight in children.items():
-#             die += [child] * weight
-#             total_weight += weight
-#         node = die[randint(0, total_weight-1)]
-#     return text+' '
-#
-#
-# while True:
-#     command = input('Input a command: ')
-#     if command in {'edges', 'e'}:
-#         node = input('edges from node: ')
-#         try:
-#             print(chains.get_neighbors(node))
-#         except KeyError:
-#             print('Node %s not in chains' % node)
-#     elif command in {'nodes', 'n'}:
-#         print(chains.get_nodes())
-#     elif command in {'text', 't'}:
-#         max_length = input('maximum length: ')
-#         print(gen_text(chains))
-#     elif command in {'quit', 'exit', 'q'}:
-#         break
-#     else:
-#         print('Command not recognized')
+def gen_text(graph, max_length=100):
+    node = START_TOKEN
+    text = ''
+    count = 0
+    while node != END_TOKEN and count < max_length:
+        count += 1
+        if node != START_TOKEN:
+            text += node+' '
+        children = graph.get_neighbors(node)
+        total_weight = 0
+        die = []
+        for child, weight in children.items():
+            die += [child] * weight
+            total_weight += weight
+        node = die[randint(0, total_weight-1)]
+    return text+' '
+
+
+while True:
+    command = input('Input a command: ')
+    if command in {'edges', 'e'}:
+        node = input('edges from node: ')
+        try:
+            print(chains.get_neighbors(node))
+        except KeyError:
+            print('Node %s not in chains' % node)
+    elif command in {'nodes', 'n'}:
+        print(chains.get_nodes())
+    elif command in {'text', 't'}:
+        max_length = input('maximum length: ')
+        print(gen_text(chains))
+    elif command in {'quit', 'exit', 'q'}:
+        break
+    else:
+        print('Command not recognized')
