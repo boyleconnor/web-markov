c31d00491286a28f854b284b727d36b873fb7721
cascadianblue <connor.bo@gmail.com>
2016-11-23 10:34:14 -0600

    Change to Allow N-grams

--- graph.py
+++ graph.py
@@ -1,37 +1,37 @@
 class Graph:
     '''
-    Directional, weighted graph whose nodes are strings, and whose edge weights
-    are integers.
+    Directional, weighted graph whose nodes are strings or tuples of strings,
+    and whose edge weights are integers.
     '''
     def __init__(self):
         self.nodes = set()
         self.edges = {}
 
-    def add_node(self, node):
-        '''
-        Assumes <node> has not already been added to this graph
-        '''
-        self.nodes.add(node)
-
     def add_edge(self, node_one, node_two):
         '''
-        Adds an edge from node_one to node_two if none exists, otherwise
-        increment the weight of the existing one
+        Increments the possibility of a path from node_one to node_two
         '''
-        # Sanity check
-        assert node_one in self.nodes and node_two in self.nodes
+        self.nodes.add(node_one)
+        self.nodes.add(node_two)
 
         if node_one in self.edges:
             if node_two in self.edges[node_one]:
-                self.edges[node_one][node_two] += 1 # = self.edges[node_one][node_two] + 1
+                # Increment adjacency from node_one to node_two
+                self.edges[node_one][node_two] += 1
             else:
+                # Create new adjacency from node_one to node_two
                 self.edges[node_one][node_two] = 1
         else:
-            # Create new adjacency table for node_one
+            # Create new adjacency table for node_one,
+            # populate with new adjacency to node_two
             self.edges[node_one] = {node_two: 1}
 
     def get_nodes(self):
         return self.nodes
 
-    def get_neighbors(self, node):
+    def get_children(self, node):
+        '''
+        Return a dict of all of the children of <node> mapped to the weight of
+        the edge leading to them.
+        '''
         return self.edges[node]
--- onechains.py
+++ onechains.py
@@ -1,11 +1,13 @@
 import re
-from random import randint
+import random
 from graph import Graph
 
 
 
 START_TOKEN = '<START>'
 END_TOKEN = '<END>'
+
+NGRAM_SIZE = 3
 
 def read_text(text_file_path):
     with open(text_file_path) as text_file:
@@ -17,13 +19,31 @@ def read_text(text_file_path):
             text += START_TOKEN+' '+tokens[1]+' '+END_TOKEN+'\n'
     return text
 
-def gen_graph(text):
+
+def gen_graph(text, n):
+    '''
+    Generate a graph of n-grams from the text
+    '''
+    if n < 1:
+        raise ValueError("n must be greater than or equal to 1")
+    elif type(n) != int:
+        raise TypeError("n must be an integer")
     chains = Graph()
-    tokens = text.split()
-    for i in range(len(tokens)-1):
-        chains.add_node(tokens[i])
-        chains.add_node(tokens[i+1])
-        chains.add_edge(tokens[i], tokens[i+1])
+    lines = text.split('\n')
+    for line in lines:
+        tokens = line.split()
+        node = tuple()
+        for x in range(n):
+            try:
+                new_node = node + (tokens[x],)
+            except IndexError:
+                break
+            chains.add_edge(node, new_node)
+            node = new_node
+        for i in range(len(tokens)-n):
+            node_one = tuple([tokens[i+j] for j in range(n)])
+            node_two = tuple([tokens[i+j+1] for j in range(n)])
+            chains.add_edge(node_one, node_two)
     return chains
 
 
@@ -32,34 +52,42 @@ def gen_random_text(graph, max_length=100):
     Return an array of tokens (words) that are statistically likely given the
     Markov Chains stored in <graph>
     '''
-    node = START_TOKEN
+    node = (START_TOKEN,)
     text = []
     count = 0
     while node != END_TOKEN and count <= max_length:
         count += 1
-        if node != START_TOKEN:
-            text += [node]
-        children = graph.get_neighbors(node)
+        if END_TOKEN in node:
+            break
+        elif node != (START_TOKEN,):
+            text += [node[-1]]
+        children = graph.get_children(node)
         total_weight = 0
         die = []
         for child, weight in children.items():
             die += [child] * weight
             total_weight += weight
-        node = die[randint(0, total_weight-1)]
+        node = random.choice(die)
     return text
 
 if __name__ == '__main__':
     DATA_FILE = "Tweet Databases/tweetDatabase_Trump"
     text = read_text(DATA_FILE)
-    chains = gen_graph(text)
+    chains = gen_graph(text, NGRAM_SIZE)
     while True:
         command = input('Input a command: ')
         if command in {'edges', 'e'}:
-            node = input('edges from node: ')
+            node = tuple()
+            for x in range(NGRAM_SIZE):
+                token = input("token %s: " % (x+1))
+                if token == '':
+                    break
+                node += (token,)
             try:
-                print(chains.get_neighbors(node))
+                children = chains.get_children(node)
+                print((node, children))
             except KeyError:
-                print('Node %s not in chains' % node)
+                print('Node %s not in chains' % str(node))
         elif command in {'nodes', 'n'}:
             print(chains.get_nodes())
         elif command in {'text', 't'}:
