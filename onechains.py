5bf7c1b19a0b528323a3db4fb6d7e466280e3783
cascadianblue <connor.bo@gmail.com>
2016-12-04 20:33:41 -0600

    Revert onechains.py to its Former Glory
    The merge accidentally brought in my changes to onechains.py from the
    branch 'refactor'. Whoops! This commit fixes that.

--- onechains.py
+++ onechains.py
@@ -1,100 +1,66 @@
 #TwitterBot
 #n=1
 import re
-import random
+from random import randint
 from graph import Graph
 
 
+chains = Graph()
 
 START_TOKEN = '<START>'
 END_TOKEN = '<END>'
+DATA_FILE = "Tweet Databases/tweetDatabase_Trump"
+
+with open(DATA_FILE) as f:
+    text = ''
+    count = 0
+    for line in f:
+        count += 1
+        tokens = re.findall('[^\]\[]+', line)
+        text += START_TOKEN+' '+tokens[1]+' '+END_TOKEN+'\n'
+
+tokens = text.split()
+for i in range(len(tokens)-1):
+    chains.add_node(tokens[i])
+    chains.add_node(tokens[i+1])
+    chains.add_edge(tokens[i], tokens[i+1])
 
 
-def read_text(text_file_path):
-    with open(text_file_path) as text_file:
-        text = ''
-        count = 0
-        for line in text_file:
-            count += 1
-            tokens = re.findall('[^\]\[]+', line)
-            text += START_TOKEN+' '+tokens[1]+' '+END_TOKEN+'\n'
-    return text
-
-
-def gen_graph(text, n):
-    '''
-    Generate a graph of n-grams from the text
-    '''
-    if n < 1:
-        raise ValueError("n must be greater than or equal to 1")
-    elif type(n) != int:
-        raise TypeError("n must be an integer")
-    chains = Graph()
-    lines = text.split('\n')
-    for line in lines:
-        tokens = line.split()
-        node = tuple()
-        for x in range(n):
-            try:
-                new_node = node + (tokens[x],)
-            except IndexError:
-                break
-            chains.add_edge(node, new_node)
-            node = new_node
-        for i in range(len(tokens)-n):
-            node_one = tuple([tokens[i+j] for j in range(n)])
-            node_two = tuple([tokens[i+j+1] for j in range(n)])
-            chains.add_edge(node_one, node_two)
-    return chains
-
-
-def gen_random_text(graph, max_length=100):
+def gen_text(graph, max_length=100):
     '''
     Return an array of tokens (words) that are statistically likely given the
     Markov Chains stored in <graph>
     '''
-    node = (START_TOKEN,)
+    node = START_TOKEN
     text = []
     count = 0
     while node != END_TOKEN and count <= max_length:
         count += 1
-        if END_TOKEN in node:
-            break
-        elif node != (START_TOKEN,):
-            text += [node[-1]]
-        children = graph.get_children(node)
+        if node != START_TOKEN:
+            text += [node]
+        children = graph.get_neighbors(node)
         total_weight = 0
         die = []
         for child, weight in children.items():
             die += [child] * weight
             total_weight += weight
-        node = random.choice(die)
+        node = die[randint(0, total_weight-1)]
     return text
 
 if __name__ == '__main__':
-    DATA_FILE = "Tweet Databases/tweetDatabase_Trump"
-    ngram_size = int(input('Pick n-gram size: '))
-    text = read_text(DATA_FILE)
-    chains = gen_graph(text, ngram_size)
     while True:
         command = input('Input a command: ')
         if command in {'edges', 'e'}:
-            node = tuple()
-            for x in range(ngram_size):
-                token = input("token %s: " % (x+1))
-                if token == '':
-                    break
-                node += (token,)
+            node = input('edges from node: ')
             try:
-                children = chains.get_children(node)
-                print((node, children))
+                print(chains.get_neighbors(node))
             except KeyError:
-                print('Node %s not in chains' % str(node))
+                print('Node %s not in chains' % node)
         elif command in {'nodes', 'n'}:
             print(chains.get_nodes())
         elif command in {'text', 't'}:
             max_length = int(input('maximum length: '))
-            text = gen_random_text(chains, max_length)
+            text = gen_text(chains, max_length)
             print(' '.join(text))
         elif command in {'quit', 'exit', 'q'}:
             break
