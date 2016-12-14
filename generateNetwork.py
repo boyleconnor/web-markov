4e4373d8e0a055e1783e1c124729e9e61a6d1bec
Ayoub Belemlih <abelemli@macalester.edu>
2016-12-13 23:08:15 -0600

    Edited network visualization code

--- generateNetwork.py
+++ generateNetwork.py
@@ -1,25 +1,25 @@
 
 import nchains
 
-DATA_FILE = "tweet_databases/tweetDatabase_elonmusk"
+DATA_FILE = "tweet_databases/tweetDatabase_Trump"
 network = nchains.gen_graph(nchains.read_text(DATA_FILE), 2)
 tweet_nodes = nchains.gen_random_path(network)
 print(tweet_nodes)
 tweet_network = []
-for i in range(1,len(tweet_nodes)-1):
+for i in range(0,len(tweet_nodes)-1):
     neighbors = network.get_children(tweet_nodes[i])
     for y in neighbors:
         if (i <= len(tweet_nodes)-3 and y==tweet_nodes[i+1]):
             node_tuple = tweet_nodes[i]
-            node = node_tuple[0] + ' ' + node_tuple[1]
-            connected_neighbor = y[0] + ' ' + y[1]
+            node = ' '.join(node_tuple)
+            connected_neighbor = ' '.join(y)
             weight_edge = neighbors[y]
             connection_str = '{"' + node + '"->"' + connected_neighbor + '",' + 'Blue-'+ str(weight_edge) + '}'
             tweet_network.append(connection_str)
         else:
             node_tuple = tweet_nodes[i]
-            node = node_tuple[0] + ' ' + node_tuple[1]
-            connected_neighbor = y[0] + ' ' + y[1]
+            node = ' '.join(node_tuple)
+            connected_neighbor = ' '.join(y)
             weight_edge = neighbors[y]
             connection_str = '{"' + node + '"->"' + connected_neighbor + '",' + str(weight_edge) + '}'
             tweet_network.append(connection_str)
