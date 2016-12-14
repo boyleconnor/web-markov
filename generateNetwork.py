c2e63501ad7418a2a27fc6fa2c49fa5178321f91
Ayoub Belemlih <abelemli@macalester.edu>
2016-12-13 22:53:47 -0600

    Edited network visualization code

--- generateNetwork.py
+++ generateNetwork.py
@@ -1,31 +1,32 @@
-from graph import Graph
-import re
-from random import randint
+
 import nchains
 
 DATA_FILE = "tweet_databases/tweetDatabase_elonmusk"
-network = nchains.gen_graph(nchains.read_text(DATA_FILE),2)
-tweet_words = nchains.gen_random_text(network,50) #tweet is a list of words
-print(tweet_words)
+network = nchains.gen_graph(nchains.read_text(DATA_FILE), 2)
+tweet_nodes = nchains.gen_random_path(network)
+print(tweet_nodes)
 tweet_network = []
-for i in range(0,len(tweet_words)-1):
-    neighbors = network.get_children((tweet_words[i],tweet_words[i+1]))
+for i in range(1,len(tweet_nodes)-1):
+    neighbors = network.get_children(tweet_nodes[i])
     for y in neighbors:
-        if (i <= len(tweet_words)-3 and y==(tweet_words[i+1],tweet_words[i+2])):
-            node = tweet_words[i] + ' ' + tweet_words[i + 1]
+        if (i <= len(tweet_nodes)-3 and y==tweet_nodes[i+1]):
+            node_tuple = tweet_nodes[i]
+            node = node_tuple[0] + ' ' + node_tuple[1]
             connected_neighbor = y[0] + ' ' + y[1]
             weight_edge = neighbors[y]
             connection_str = '{"' + node + '"->"' + connected_neighbor + '",' + 'Blue-'+ str(weight_edge) + '}'
             tweet_network.append(connection_str)
         else:
-            node = tweet_words[i] + ' ' + tweet_words[i + 1]
+            node_tuple = tweet_nodes[i]
+            node = node_tuple[0] + ' ' + node_tuple[1]
             connected_neighbor = y[0] + ' ' + y[1]
             weight_edge = neighbors[y]
             connection_str = '{"' + node + '"->"' + connected_neighbor + '",' + str(weight_edge) + '}'
             tweet_network.append(connection_str)
 
-dbase = open("sample_tweet_words",'w')
+dbase = open("NetworkBuilder/tweet_path_mathematica",'w')
+dbase.write("GraphPlot[{")
 for x in tweet_network:
     dbase.write(x)
     dbase.write(',')
-
+dbase.write("VertexLabeling -> True]")
