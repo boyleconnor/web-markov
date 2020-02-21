2aa318ef510d890230c8ce30fd3d5bb3cf5e0df7
Connor Boyle <connor.bo@gmail.com>
2020-02-20 22:38:16 -0800

    Add Tests for get_ngrams Method
    Also, update tests for add_ngram. It was not testing enough.

--- tests/test_ngram.py
+++ tests/test_ngram.py
@@ -9,8 +9,31 @@ class NGramTest(TestCase):
     def test_add_ngram(self):
         ngram = NGram(3)
         ngram.add_ngram('hello', 'there', 'sir')
+        ngram.add_ngram('hello', 'there', 'sir')
+        ngram.add_ngram('hello', 'there', 'man')
+        ngram.add_ngram('hello', 'mister', 'man')
+        self.assertIn('sir', ngram.get_ngrams('hello', 'world'))
 
     def test_add_wrong_length_ngram(self):
         ngram = NGram(3)
         with self.assertRaises(ValueError) as context:
             ngram.add_ngram('hello', 'world')
+
+    def test_get_ngrams(self):
+        ngram = NGram(3)
+        ngram.add_ngram('hello', 'there', 'sir')
+        ngram.add_ngram('hello', 'there', 'sir')
+        ngram.add_ngram('hello', 'there', 'world')
+        ngram.add_ngram('hello', 'mister', 'Smith')
+        ngram.add_ngram('hello', 'mister', 'Boyle')
+
+        hello_mister = self.get_ngrams('hello', 'there')
+        hello_mister_boyle = ('hello', 'mister', 'Boyle')
+        self.assertIn(hello_mister_boyle, hellow_mister)
+        self.assertAlmostEqual(hello_mister[hello_mister_boyle], 1 / 2)
+
+        hello_there = self.get_ngrams('hello', 'there')
+        hello_there_sir = ('hello', 'there', 'sir')
+        self.assertIn(hello_there_sir, hello_there)
+        self.assertAlmostEqual(hello_there[hello_there_sir], 2 / 3)
+        self.assertNotIn(hello_mister_boyle, hello_there)
