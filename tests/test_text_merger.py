40adf16fb340b9cfd858721e3144fb66dfdac720
Connor Boyle <connor.bo@gmail.com>
2020-02-28 12:07:42 -0800

    Add Test for TextMerger.get_bias Method

--- tests/test_text_merger.py
+++ tests/test_text_merger.py
@@ -5,11 +5,14 @@ from text_merger import TextMerger
 SOURCE_ONE = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "Donec ut maximus ante.",
+    "Nam nec mattis diam.",
     "Etiam lacinia massa dui, pretium iaculis risus ultrices in.",
     "Ut mi urna, fringilla vel justo sed, fringilla pellentesque nulla.",
     "Ut condimentum facilisis lorem, ac eleifend tortor varius et.",
     "Integer porttitor orci nec porttitor ullamcorper.",
     "Nullam non molestie nibh, varius rutrum mi."
+    "Aliquam ut purus eu leo aliquam sodales.",
+    "Aliquam ut purus lui leo aliquam sodales.",
 ]
 SOURCE_TWO = [
     "Ut quis elementum nisi.",
@@ -27,3 +30,18 @@ SOURCE_TWO = [
 class TextMergerTests(TestCase):
     def test_initialize(self):
         text_merger = TextMerger(3, SOURCE_ONE, SOURCE_TWO)
+
+    def test_get_bias(self):
+        text_merger = TextMerger(3, SOURCE_ONE, SOURCE_TWO)
+        self.assertAlmostEqual(text_merger.get_bias('Ut', ' ','quis'), -1.0)
+        self.assertAlmostEqual(text_merger.get_bias('Donec', ' ','ut'), 1.0)
+        self.assertAlmostEqual(text_merger.get_bias('mattis', ' ','diam'), 0.5)
+        self.assertAlmostEqual(text_merger.get_bias('ut', ' ','purus'), 1 / 3)
+        self.assertAlmostEqual(text_merger.get_bias('Nam', ' ', 'nec'), 0.0)
+
+    def test_get_properties(self):
+        text_merger = TextMerger(3, SOURCE_ONE, SOURCE_TWO)
+        sequence = ('', '', 'Donec', ' ', 'ut', ' ', 'purus', ' ', 'eu', ' ',
+                    'leo', ' ', 'aliquam', ' ', 'sodales', '.', '')
+        properties = text_merger.get_properties(*sequence)
+        print(properties)
