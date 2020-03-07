48ed8eb40fd111e49ee30a18e41ecd87a9a0c29a
Connor Boyle <connor.bo@gmail.com>
2020-03-06 21:48:56 -0800

    DRY Out User Input Methods
    DRY stands for Don't Repeat Yourself--advice we should all be following
    in our daily lives!

--- utils/user_input.py
+++ utils/user_input.py
@@ -4,34 +4,31 @@ import os
 SOURCE_DIR = 'sources'
 
 
-def get_n(default_n):
+def get_integer(default_value, name, minimum):
     while True:
         try:
-            n = input('Input n-gram size (default = %d): ' % (default_n,))
-            if n == '':
-                return default_n
-            n = int(n)
-            if n < 2:
+            number = input('Input %s (default = %d): ' % (name, default_value,))
+            if number == '':
+                return default_value
+            number = int(number)
+            if number < minimum:
                 raise ValueError()
         except ValueError:
-            print("n must be an integer greater or equal to 2")
+            print("Input must be an integer greater or equal to %d" % (minimum,))
         else:
-            return n
+            return number
+
+
+def get_n(default_n):
+    return get_integer(default_n, 'n-gram size', 2)
 
 
 def get_iterations(default_iterations):
-    while True:
-        try:
-            n = input('Input number of iterations (default = %d): ' % (default_iterations,))
-            if n == '':
-                return default_iterations
-            n = int(n)
-            if n < 1:
-                raise ValueError()
-        except ValueError:
-            print("iterations must be an integer greater or equal to 1")
-        else:
-            return n
+    return get_integer(default_iterations, 'number of iterations', 1)
+
+
+def get_sample_size(default_sequences):
+    return get_integer(default_sequences, 'sample size', 1)
 
 
 def get_source_path(default_source):
