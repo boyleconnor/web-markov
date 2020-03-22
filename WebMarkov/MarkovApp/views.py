9a021ee51d51f30a16f0504f1d5e96060d553ea4
Connor Boyle <connor.bo@gmail.com>
2020-03-21 23:02:40 -0700

    Add Upload and Home Views

--- WebMarkov/MarkovApp/views.py
+++ WebMarkov/MarkovApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from MarkovApp.models import SingleMarkov, MergedMarkov
+from MarkovApp.forms import SingleMarkovForm
 
-# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        # Send (up to) 5 Merged & Single Markovs to the template
+        # FIXME: Think of ordering them
+        context['single_markovs'] = SingleMarkov.objects.all()[:5]
+        context['merged_markovs'] = MergedMarkov.objects.all()[:5]
+
+        return context
+
+
+class UploadSource(CreateView):
+    template_name = 'upload_source.html'
+    form_class = SingleMarkovForm
