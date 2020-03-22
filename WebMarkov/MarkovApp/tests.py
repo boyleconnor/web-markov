dfb1860a1b7368438d3dfbe3042eecbe0fed3842
Connor Boyle <connor.bo@gmail.com>
2020-03-21 21:38:48 -0700

    Change MarkovApp Tests to Read Files in Bytes Mode
    MarkovApp now reads tests in bytes mode, in order to better emulate a
    production environment. Tests will probably fail as of this commit.

--- WebMarkov/MarkovApp/tests.py
+++ WebMarkov/MarkovApp/tests.py
@@ -6,14 +6,14 @@ from django.core.files import File
 
 class SingleMarkovTest(TestCase):
     def test_initializer(self):
-        with open('MarkovMerge/sources/hamlet.txt') as hamlet:
+        with open('MarkovMerge/sources/hamlet.txt', 'rb') as hamlet:
             hamlet_file = File(hamlet, name='hamlet.txt')
             single_markov = SingleMarkov.objects.create(source_file=hamlet_file)
         single_markov.source_file.delete()
         single_markov.delete()
 
     def test_generate_text(self):
-        with open('MarkovMerge/sources/trump.txt') as trump:
+        with open('MarkovMerge/sources/trump.txt', 'rb') as trump:
             trump_file = File(trump, name='trump.txt')
             markov_one = SingleMarkov.objects.create(source_file=trump_file)
         random_text = markov_one.markov_model.random_text()
@@ -22,7 +22,7 @@ class SingleMarkovTest(TestCase):
         markov_one.delete()
 
     def test_get_from_db(self):
-        with open('MarkovMerge/sources/frankenstein.txt') as frankenstein:
+        with open('MarkovMerge/sources/frankenstein.txt', 'rb') as frankenstein:
             frankenstein_file = File(frankenstein, name='frankenstein.txt')
             SingleMarkov.objects.create(source_file=frankenstein_file)
 
