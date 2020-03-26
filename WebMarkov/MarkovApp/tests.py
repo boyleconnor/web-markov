5b8520d14665326a4200dd29dbef22abcc253b94
Connor Boyle <connor.bo@gmail.com>
2020-03-25 21:17:57 -0700

    Add Tests for MergedMarkov

--- WebMarkov/MarkovApp/tests.py
+++ WebMarkov/MarkovApp/tests.py
@@ -30,3 +30,32 @@ class SingleMarkovTest(TestCase):
             frankenstein_markov = SingleMarkov.objects.get()
             self.assertEqual(frankenstein_markov.pk, 1)
             frankenstein_source.source_file.delete()
+
+
+class MergedMarkovTests(TestCase):
+    def test_initializer(self):
+        hamlet_file = open('MarkovMerge/sources/hamlet.txt', 'rb')
+        mock_hamlet = File(hamlet_file, name='hamlet.txt')
+        hamlet_source = Source.objects.create(source_file=mock_hamlet, name='Hamlet')
+
+        trump_file  = open('MarkovMerge/sources/trump.txt', 'rb')
+        mock_trump = File(trump_file, name='trump.txt')
+        trump_source = Source.objects.create(source_file=mock_trump, name='Trump tweets')
+
+        merged = MergedMarkov.objects.create(source_one=trump_source, source_two=hamlet_source)
+        hamlet_source.source_file.delete()
+        trump_source.source_file.delete()
+
+    def test_generate_sequence(self):
+        hamlet_file = open('MarkovMerge/sources/hamlet.txt', 'rb')
+        mock_hamlet = File(hamlet_file, name='hamlet.txt')
+        hamlet_source = Source.objects.create(source_file=mock_hamlet, name='Hamlet')
+
+        trump_file  = open('MarkovMerge/sources/trump.txt', 'rb')
+        mock_trump = File(trump_file, name='trump.txt')
+        trump_source = Source.objects.create(source_file=mock_trump, name='Trump tweets')
+
+        merged = MergedMarkov.objects.create(source_one=trump_source, source_two=hamlet_source)
+        random_text = merged.markov_model.random_sequence()
+        merged.source_one.source_file.delete()
+        merged.source_two.source_file.delete()
