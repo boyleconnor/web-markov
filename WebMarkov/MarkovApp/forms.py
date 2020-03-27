e3753f86d16b250bbb0e2a51b270d7d0595b5605
Connor Boyle <connor.bo@gmail.com>
2020-03-27 11:08:40 -0700

    Add MergedMarkovForm

--- WebMarkov/MarkovApp/forms.py
+++ WebMarkov/MarkovApp/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from MarkovApp.models import Source, SingleMarkov
+from MarkovApp.models import Source, SingleMarkov, MergedMarkov
 
 
 class SourceForm(ModelForm):
@@ -12,3 +12,9 @@ class SingleMarkovForm(ModelForm):
     class Meta:
         model = SingleMarkov
         fields = ['source', 'ngram_size']
+
+
+class MergedMarkovForm(ModelForm):
+    class Meta:
+        model = MergedMarkov
+        fields = ['source_one', 'source_two', 'ngram_size']
