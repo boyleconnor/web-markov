c67ad0ea01f738959294870bbc19c86994e54563
Connor Boyle <connor.bo@gmail.com>
2020-04-03 17:08:32 -0700

    Start DB-ifying the Models
    I'm trying to store the model-prefix-suffix graph relation in the
    database. It is extremely slow so far, but hopefully I can make it
    faster by doing a lot of the processing in-memory (ideally in parallel)
    before committing to the database.

--- WebMarkov/MarkovApp/tests.py
+++ WebMarkov/MarkovApp/tests.py
@@ -1,61 +1,55 @@
 from django.test import TestCase
-from MarkovApp.models import Source, SingleMarkov, MergedMarkov
+from MarkovApp.models import Source, Markov
 from django.core.files import File
 
-# Create your tests here.
 
-class SingleMarkovTest(TestCase):
-    def test_initializer(self):
-        with open('MarkovMerge/sources/hamlet.txt', 'rb') as hamlet_file:
-            mock_file = File(hamlet_file, name='hamlet.txt')
-            hamlet_source = Source.objects.create(source_file=mock_file, name="Shakespeare's Hamlet")
-            hamlet_markov = SingleMarkov.objects.create(source=hamlet_source)
-            hamlet_source.source_file.delete()
+class DeterministicMarkovTests(TestCase):
+    def test_read_text(self):
+        markov = Markov.objects.create()
+        markov.read_text('hello world')
+        self.assertTrue(markov.prefixes.filter(tokens='').exists())
+        start_of_text = markov.prefixes.get(tokens='')
+        self.assertEqual(start_of_text.suffixes.get().token, 'hello')
+        self.assertEqual(start_of_text.suffixes.get().weight, 1)
+        markov.read_text('hello world')
+        markov.read_text('hello friend')
+        hello__prefix = markov.prefixes.get(tokens='hello ')
+        self.assertEqual(hello__prefix.suffixes.count(), 2)
+        self.assertEqual(hello__prefix.occurrences, 3)
+        self.assertEqual(hello__prefix.suffixes.get(token='world').weight, 2)
+        self.assertEqual(hello__prefix.suffixes.get(token='friend').weight, 1)
 
-    def test_generate_text(self):
+    def test_train_on_source(self):
         with open('MarkovMerge/sources/trump.txt', 'rb') as trump_file:
             mock_file = File(trump_file, name='trump.txt')
             trump_source = Source.objects.create(source_file=mock_file, name="Trump's Twitter")
-            trump_markov = SingleMarkov.objects.create(source=trump_source)
-            random_text = trump_markov.markov_model.random_text()
-            self.assertGreater(len(random_text), 1)
+            markov = Markov.objects.create(name="TrumpBot")
+            # markov.train_on(trump_source)  # FIXME: This is still way too slow.
+            # self.assertIn(trump_source, markov.trained_on.all())
+            # print(markov.random_text())
             trump_source.source_file.delete()
 
-    def test_get_from_db(self):
-        with open('MarkovMerge/sources/frankenstein.txt', 'rb') as frankenstein_file:
-            mock_file = File(frankenstein_file, name='frankenstein.txt')
-            Source.objects.create(source_file=mock_file, name='Frankenstein')
-            frankenstein_source = Source.objects.get()
-            SingleMarkov.objects.create(source=frankenstein_source)
-            frankenstein_markov = SingleMarkov.objects.get()
-            self.assertEqual(frankenstein_markov.pk, 1)
-            frankenstein_source.source_file.delete()
 
+class NonDeterministicMarkovTests(TestCase):
+    '''These tests depend on random behavior. They run the functions in
+    question through thousands of iterations, so the tests are unlikely to
+    fail if the functions being tested have been implemented correctly.
+    '''
+    def test_random_text(self):
+        markov = Markov.objects.create()
+        markov.read_text('hello world')
+        markov.read_text('hello world')
+        markov.read_text('hello friend')
 
-class MergedMarkovTests(TestCase):
-    def test_initializer(self):
-        hamlet_file = open('MarkovMerge/sources/hamlet.txt', 'rb')
-        mock_hamlet = File(hamlet_file, name='hamlet.txt')
-        hamlet_source = Source.objects.create(source_file=mock_hamlet, name='Hamlet')
-
-        trump_file  = open('MarkovMerge/sources/trump.txt', 'rb')
-        mock_trump = File(trump_file, name='trump.txt')
-        trump_source = Source.objects.create(source_file=mock_trump, name='Trump tweets')
-
-        merged = MergedMarkov.objects.create(source_one=trump_source, source_two=hamlet_source)
-        hamlet_source.source_file.delete()
-        trump_source.source_file.delete()
-
-    def test_generate_sequence(self):
-        hamlet_file = open('MarkovMerge/sources/hamlet.txt', 'rb')
-        mock_hamlet = File(hamlet_file, name='hamlet.txt')
-        hamlet_source = Source.objects.create(source_file=mock_hamlet, name='Hamlet')
-
-        trump_file  = open('MarkovMerge/sources/trump.txt', 'rb')
-        mock_trump = File(trump_file, name='trump.txt')
-        trump_source = Source.objects.create(source_file=mock_trump, name='Trump tweets')
-
-        merged = MergedMarkov.objects.create(source_one=trump_source, source_two=hamlet_source)
-        random_text = merged.markov_model.random_sequence()
-        merged.source_one.source_file.delete()
-        merged.source_two.source_file.delete()
+        world_count = 0
+        friend_count = 0
+        ITERATIONS = 100
+        for i in range(ITERATIONS):
+            random_text = markov.random_text()
+            self.assertIn(random_text, {'hello world', 'hello friend'})
+            if random_text == 'hello world':
+                world_count += 1
+            elif random_text == 'hello friend':
+                friend_count += 1
+        self.assertAlmostEqual(world_count / ITERATIONS, 2 / 3, 1)
+        self.assertAlmostEqual(friend_count / ITERATIONS, 1 / 3, 1)
