768d156878aa59f9314efbb08036f12054bc9cba
Connor Boyle <connor.bo@gmail.com>
2020-04-12 19:25:38 -0700

    Switch Server to Sockets-based Comm Protocol
    Server now uses an application protocol built on top of sockets.

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import json
 import os
+import socket
 import sys
 from .views import router
 from .manager import Manager
@@ -16,11 +17,39 @@ class Server:
         # Load active models into memory
         self.manager = Manager(self.config['data_directory'])
 
+        # Attach views
         self.router = router
 
+        # Set up network server
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host_name, port = self.config['host_name'], self.config['port']
+        self.sock.bind((host_name, port))
+        self.sock.listen(5)
+
+    def get_request(self):
+        '''Get the next request. This method returns a pair: (connection,
+        request), where connection is a socket and request is the contents of
+        the request (parsed from JSON into into a Python object).
+        '''
+        connection, address = self.sock.accept()
+        print("Accepting connection from %s:%d" % address)
+        request_string = ""
+        while True:
+            chunk = connection.recv(1024)
+
+            # An empty chunk will be received when the client starts shutting
+            # down the connection; that means we have received the whole
+            # request.
+            if not chunk:
+                break
+
+            request_string += chunk.decode('utf-8')  # FIXME: Hard-coding as UTF-8: a good idea?
+
+        return connection, json.loads(request_string)
+
     def run(self):
-        for line in sys.stdin:  # FIXME: This should be generalized to other input sources
-            request = json.loads(line)
+        while True:
+            connection, request = self.get_request()
             command = request['command']
             args = request.get('args', {})
             try:
@@ -35,4 +64,5 @@ class Server:
                     'status': 'error',
                     'result': str(e)
                 })
-            print(response)  # FIXME: This should be generalized to other outputs
+            connection.sendall(response.encode('utf-8'))
+            connection.close()
