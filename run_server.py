d856c82f5ac192101619a4d2992abbc6b789c690
Connor Boyle <connor.bo@gmail.com>
2020-04-21 02:06:28 -0700

    Require Python3 for to Run MarkovMerge Server

--- run_server.py
+++ run_server.py
@@ -1,9 +1,14 @@
 import argparse
+import sys
 from server.server import Server
 from server.views import router
 
 
 if __name__ == '__main__':
+    if sys.version_info[0] != 3:
+        print("This script and package require Python 3")
+        exit()
+
     parser = argparse.ArgumentParser(description='Load and serve markov models')
     parser.add_argument('config_file', metavar='CONFIG_FILE', type=str)
     args = parser.parse_args()
