daa0040578e98a43bf5436eecb2c0a03316cad79
Connor Boyle <connor.bo@gmail.com>
2020-05-07 20:22:08 -0700

    Change Models to Use DB as Backend
    The "Markov" model in the "markov" app no longer uses ICP to interact
    with a markov model stored in some other backend service. Markov models
    are now stored purely in the database. We will see if we can get this to
    work efficiently.

--- web_markov/markov/models.py
+++ web_markov/markov/models.py
@@ -1,24 +1,18 @@
 import random
 import re
 from collections import deque
-from io import TextIOWrapper
 from django.db import models
 from django.conf import settings
-from .client import Client
-
-
-
-# Connect to the MarkovMerge server
-host_name, port = settings.MARKOV_MERGE_SERVER_ADDRESS
-client = Client(host_name, port)
+from picklefield.fields import PickledObjectField
 
 
 class Source(models.Model):
     name = models.CharField(max_length=100, blank=False, unique=True)
-    source_file = models.FileField(upload_to='sources/')
+    file = models.FileField(upload_to='sources/')
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=True  # FIXME: Null is allowed solely for testing purposes
     )
 
     def __str__(self):
@@ -37,43 +31,111 @@ class Markov(models.Model):
     n = models.PositiveSmallIntegerField(default=5)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=True  # FIXME: Just for testing, don't allow null in production
     )
-    saved_to_markov_merge = models.BooleanField(default=False)
+    graph = PickledObjectField(default=dict)
+
+    def _add_ngram(self, prefix, suffix, weight=1):
+        '''Record the occurrence of the given n-gram.
+        '''
+        if prefix not in self.graph:
+            self.graph[prefix] = {suffix: weight}
+        elif suffix not in self.graph[prefix]:
+            self.graph[prefix][suffix] = weight
+        else:
+            self.graph[prefix][suffix] += weight
+
+    def _tokenize(self, text):
+        return re.findall(self.tokenizer, text)
+
+    def read_text(self, text, weight=1):
+        '''Read a text as a series of ngrams. This method treats the beginning
+        of a text as (n-1) blank tokens (i.e. [""] * n-1) and the end of text
+        as a single blank token. Texts can be any number of tokens in length
+        (i.e. a text does NOT need to have a minimum of n tokens).
+
+        An inputted text will be tokenized using the callback tokenize. If no
+        tokenize function is provided, the text will be split along word
+        boundaries (i.e. the punctuation and whitespace between any two words
+        is grouped together as one token).
+        '''
+        prefix_length = self.n - 1
+
+        tokens = (prefix_length * ['']) + self._tokenize(text) + ['']
+
+        for i in range(len(tokens) - prefix_length):
+            prefix = ''.join(tokens[i:i+prefix_length])
+            suffix = tokens[i+prefix_length]
+            self._add_ngram(prefix, suffix, weight)
+
+    def get_suffixes(self, prefix):
+        '''For a given prefix, get a mapping of suffixes to probabilities (i.e.
+        relative weights, as a fraction of 1).
+        '''
+        total_weight = sum(self.graph[prefix].values())
+        return {suffix: (weight / total_weight) for (suffix, weight) in self.graph[prefix].items()}
+
+    def random_suffix(self, prefix):
+        '''Return a random suffix for the given prefix; probability weighted
+        according to number of recorded occurrences.
+        '''
+        suffixes = self.get_suffixes(prefix)
+
+        threshhold = random.random()  # 0.0 < n < 1.0
+
+        up_to = 0.0
+        for suffix, probability in suffixes.items():
+            up_to += probability
+            if up_to >= threshhold:
+                return suffix
+
+    def random_sequence(self):
+        '''Probabilistically generate a sequence of tokens. Walk through the
+        model starting at recorded beginnings of texts (i.e. the prefix [''] *
+        n-1) and ending at the recorded endings of texts (i.e. the suffix '').
+        '''
+        prefix_length = self.n - 1
+        sequence = [''] * (prefix_length)
+        prefix = deque(sequence, maxlen=prefix_length)
+
+        while True:
+            prefix_string = ''.join(prefix)
+            suffix = self.random_suffix(prefix_string)
+            sequence.append(suffix)
+            prefix.append(suffix)
+            if suffix == '':
+                return sequence
 
     def random_text(self):
-        return client.random_text(str(self.pk))['result']
+        '''Probabilistically generate a text. Walk through the model starting
+        at recorded beginnings of texts (i.e. the prefix [''] * n-1) and ending
+        at the recorded endings of texts (i.e. the suffix '').
+        '''
+
+        sequence = self.random_sequence()
+        return ''.join(sequence)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        # Add model on MarkovMerge server
-        result = super().save(*args, **kwargs)
-        if not self.saved_to_markov_merge:
-            client.add_model(str(self.pk), self.n, self.tokenizer)
-            self.saved_to_markov_merge = True
-            result = super().save()
-        return result
-
-    def delete(self, *args, **kwargs):
-        client.delete_model(str(self.pk))
-        super().delete(*args, **kwargs)
 
 
 class Training(models.Model):
     markov = models.ForeignKey(Markov, on_delete=models.CASCADE, related_name='trained_on')
     source = models.ForeignKey(Source, on_delete=models.CASCADE)
-    saved_to_markov_merge = models.BooleanField(default=False)
+    weight = models.PositiveSmallIntegerField(default=1)
 
     class Meta:
         unique_together = ['markov', 'source']
 
+    def _train_model(self):
+        self.source.file.close()
+        self.source.file.open(mode='r')
+        for line in self.source.file:
+            self.markov.read_text(line)
+
     def save(self, *args, **kwargs):
-        # Train model on MarkovMerge server
-        result = super().save(*args, **kwargs)
-        if not self.saved_to_markov_merge:
-            client.train_model(str(self.markov.pk), [line.decode() for line in self.source.source_file.readlines()])
-            self.saved_to_markov_merge = True
-            result = super().save()
-        return result
+        # Actually train the model
+        if self.pk is None:
+            self._train_model()
+        return super().save(*args, **kwargs)
--- web_markov/markov/tests.py
+++ web_markov/markov/tests.py
@@ -1,34 +1,29 @@
 from django.test import TestCase
 from django.core.files import File
-from .models import Source, Markov
+from .models import Source, Markov, Training
 
 
 class DeterministicMarkovTests(TestCase):
     def test_read_text(self):
         markov = Markov.objects.create()
         markov.read_text('hello world')
-        self.assertTrue(markov.prefixes.filter(tokens='').exists())
-        start_of_text = markov.prefixes.get(tokens='')
-        self.assertEqual(start_of_text.suffixes.get().token, 'hello')
-        self.assertEqual(start_of_text.suffixes.get().weight, 1)
-        markov.read_text('hello world')
-        markov.read_text('hello friend')
-        hello__prefix = markov.prefixes.get(tokens='hello ')
-        self.assertEqual(hello__prefix.suffixes.count(), 2)
-        self.assertEqual(hello__prefix.occurrences, 3)
-        self.assertEqual(hello__prefix.suffixes.get(token='world').weight, 2)
-        self.assertEqual(hello__prefix.suffixes.get(token='friend').weight, 1)
+        self.assertIn('hello', markov.graph)
+        self.assertEqual(markov.graph['hello'], {' ': 1})
 
+    # FIXME: Rigorously test that this actually trains on the file
     def test_train_on_source(self):
         with open('../sources/trump.txt', 'rb') as trump_file:
             mock_file = File(trump_file, name='trump.txt')
-            trump_source = Source.objects.create(source_file=mock_file, name="Trump's Twitter")
+            trump_source = Source.objects.create(file=mock_file, name="Trump's Twitter")
             markov = Markov.objects.create(name="TrumpBot")
-            markov.train_on(trump_source)  # FIXME: This is still way too slow.
-            self.assertIn(trump_source, markov.trained_on.all())
-            print("Random Trump tweet:")
-            print(markov.random_text())
-            trump_source.source_file.delete()
+            Training.objects.create(markov=markov, source=trump_source)
+            trump_source.file.delete()
+
+    def test_random_sequence(self):
+        markov = Markov.objects.create(n=5, tokenizer='\w+|\W+')
+        markov.read_text('hello, world')
+        sequence = markov.random_sequence()
+        self.assertEqual(sequence, ['', '', '', '', 'hello', ', ', 'world', ''])
 
 
 class NonDeterministicMarkovTests(TestCase):
@@ -44,7 +39,7 @@ class NonDeterministicMarkovTests(TestCase):
 
         world_count = 0
         friend_count = 0
-        ITERATIONS = 100
+        ITERATIONS = 20000
         for i in range(ITERATIONS):
             random_text = markov.random_text()
             self.assertIn(random_text, {'hello world', 'hello friend'})
@@ -52,5 +47,5 @@ class NonDeterministicMarkovTests(TestCase):
                 world_count += 1
             elif random_text == 'hello friend':
                 friend_count += 1
-        self.assertAlmostEqual(world_count / ITERATIONS, 2 / 3, 1)
-        self.assertAlmostEqual(friend_count / ITERATIONS, 1 / 3, 1)
+        self.assertAlmostEqual(world_count / ITERATIONS, 2 / 3, 2)
+        self.assertAlmostEqual(friend_count / ITERATIONS, 1 / 3, 2)
