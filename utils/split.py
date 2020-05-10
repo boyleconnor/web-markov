88142ffd698442df3a99e49ca720fc9306a01d81
Connor Boyle <connor.bo@gmail.com>
2020-05-09 19:50:19 -0700

    Move Django Project to Root

--- utils/split.py
+++
@@ -1,7 +0,0 @@
-def erraticity(sequence_properties):
-    sequence, properties = sequence_properties
-    return properties['movement'] - abs(properties['net_bias'])
-
-def neutrality(sequence_properties):
-    sequence, properties = sequence_properties
-    return len(sequence[0]) - properties['total_bias'] - abs(properties['net_bias'])
